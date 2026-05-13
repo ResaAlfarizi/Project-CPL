5da6616fdde5a3a2f5ab604b2f2899b1aefbe7ab
justzaaam <nizamcahsawahan12@gmail.com>
2026-05-13 13:24:29 +0700

    style: finalize folder structure based on team request

--- apps/backend/app.js
+++ apps/backend/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 require('dotenv').config();
-const prodiRoutes = require('./src/routes/prodiRoutes');
+
+// Perhatikan: sekarang jalurnya masuk ke folder 'module1'
+const prodiRoutes = require('./module1/src/routes/prodiRoutes');
 
 const app = express();
 app.use(express.json());
 
-// Gunakan routes yang sudah dipisah
+// Menggunakan API Prodi
 app.use('/api/prodi', prodiRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Backend Modul 1 Aktif dengan Struktur Rapi!');
+  res.send('Backend Aktif - Struktur Sudah Sesuai Request Kelompok!');
 });
 
 const PORT = process.env.PORT || 3000;
